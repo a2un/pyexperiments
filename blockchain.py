ce23b22d408b78495f3a062295d38fb7b4ef3079
a2un <encodedgeek@gmail.com>
2017-10-15 08:31:27 +0530

    new files and some changes

---
+++ blockchain.py
@@ -0,0 +1,14 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+    def new_block(self):
+        pass
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+    @property
+    
